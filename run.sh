52b460ebc26b5bfb3144bafcae472b9649dc2894
Marcin Ciszak <marcin@state.com>
2017-03-22 13:30:40 +0000

    ES endpoint set via env variable

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 export HOME=/kibana
 
 : ${ELASTICSEARCH_CERTIFICATE_VERIFICATION:=true}
+: ${ELASTICSEARCH_ENDPOINT:=http://localhost:9200}
 
 sed -e "s/%ELASTICSEARCH_CERTIFICATE_VERIFICATION%/${ELASTICSEARCH_CERTIFICATE_VERIFICATION}/" \
       -i /kibana/config/kibana.yml
 
-exec /kibana/node/bin/node src/cli
+exec /kibana/node/bin/node src/cli -e ${ELASTICSEARCH_ENDPOINT}
