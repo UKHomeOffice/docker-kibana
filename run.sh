f1c551d67b14e893ce18f53026b76e9074c93a37
Marcin Ciszak <marcin@state.com>
2017-05-26 13:16:45 +0100

    Adds ES / X-pack configuration via env variables.

--- run.sh
+++ run.sh
@@ -2,9 +2,39 @@
 
 export HOME=/kibana
 
+: ${ELASTICSEARCH_URL:=http://localhost:9200}
 : ${ELASTICSEARCH_CERTIFICATE_VERIFICATION:=true}
+: ${ELASTICSEARCH_SSL_CA_CERT_PATH:=}
+: ${ELASTICSEARCH_USERNAME:=}
+: ${ELASTICSEARCH_PASSWORD:=}
+: ${XPACK_SECURITY_ENABLE:=false}
+: ${XPACK_SECURITY_ENCRYPTION_KEY:=$(uuidgen)}
+: ${XPACK_SECURITY_SECURE_COOKIES_ENABLE:=false}
+: ${XPACK_MONITORING_REPORT_STATS:=false}
 
 sed -e "s/%ELASTICSEARCH_CERTIFICATE_VERIFICATION%/${ELASTICSEARCH_CERTIFICATE_VERIFICATION}/" \
-      -i /kibana/config/kibana.yml
+    -e "s/%XPACK_SECURITY_ENABLE%/${XPACK_SECURITY_ENABLE}/" \
+    -e "s/%XPACK_SECURITY_ENCRYPTION_KEY%/${XPACK_SECURITY_ENCRYPTION_KEY}/" \
+    -e "s/%XPACK_SECURITY_SECURE_COOKIES_ENABLE%/${XPACK_SECURITY_SECURE_COOKIES_ENABLE}/" \
+    -e "s/%XPACK_MONITORING_REPORT_STATS%/${XPACK_MONITORING_REPORT_STATS}/" \
+    -i /kibana/config/kibana.yml
 
-exec /kibana/node/bin/node src/cli
+if [ -n "${ELASTICSEARCH_SSL_CA_CERT_PATH}" ]; then
+  sed -e "s#%ELASTICSEARCH_SSL_CA_CERT_PATH_CONFIG_ENTRY%#elasticsearch.ssl.ca: ${ELASTICSEARCH_SSL_CA_CERT_PATH}#" \
+      -i /kibana/config/kibana.yml
+else
+  sed -e "s/%ELASTICSEARCH_SSL_CA_CERT_PATH_CONFIG_ENTRY%//" \
+      -i /kibana/config/kibana.yml
+fi
+
+if [ -n "${ELASTICSEARCH_USERNAME}" && -n "${ELASTICSEARCH_PASSWORD}" ]; then 
+  sed -e "s#%ELASTICSEARCH_USERNAME_CONFIG_ENTRY%#elasticsearch.username: ${ELASTICSEARCH_USERNAME}#" \
+      -e "s#%ELASTICSEARCH_PASSWORD_CONFIG_ENTRY%#elasticsearch.password: ${ELASTICSEARCH_PASSWORD}#" \
+      -i /kibana/config/kibana.yml
+else
+  sed -e "s/%ELASTICSEARCH_USERNAME_CONFIG_ENTRY%//" \
+      -e "s/%ELASTICSEARCH_PASSWORD_CONFIG_ENTRY%//" \
+      -i /kibana/config/kibana.yml
+fi
+
+exec /kibana/node/bin/node src/cli -e "${ELASTICSEARCH_URL}"
