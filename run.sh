0927010db00e21fefc83e7cc0137a1a9d800663b
Marcin Ciszak <marcin@state.com>
2017-12-22 12:25:36 +0000

    Config update + extra parameter to enable impersonation proxy

--- run.sh
+++ run.sh
@@ -3,10 +3,11 @@
 export HOME=/kibana
 
 : ${ELASTICSEARCH_URL:=http://localhost:9200}
-: ${ELASTICSEARCH_CERTIFICATE_VERIFICATION:=true}
+: ${ELASTICSEARCH_CERTIFICATE_VERIFICATION:=full}
 : ${ELASTICSEARCH_SSL_CA_CERT_PATH:=}
 : ${ELASTICSEARCH_USERNAME:=}
 : ${ELASTICSEARCH_PASSWORD:=}
+: ${ENABLE_KIBANA_RUNAS_HEADER:=false}
 : ${XPACK_SECURITY_ENABLE:=false}
 : ${XPACK_SECURITY_ENCRYPTION_KEY:=$(uuidgen)}
 : ${XPACK_SECURITY_SECURE_COOKIES_ENABLE:=false}
@@ -28,10 +29,21 @@ sed -e "s/%ELASTICSEARCH_CERTIFICATE_VERIFICATION%/${ELASTICSEARCH_CERTIFICATE_V
     -i /kibana/config/kibana.yml
 
 if [[ -n "${ELASTICSEARCH_SSL_CA_CERT_PATH}" ]]; then
-  sed -e "s#%ELASTICSEARCH_SSL_CA_CERT_PATH_CONFIG_ENTRY%#elasticsearch.ssl.ca: ${ELASTICSEARCH_SSL_CA_CERT_PATH}#" \
+  sed -e "s#%ELASTICSEARCH_SSL_CA_CERT_PATH_CONFIG_ENTRY%#elasticsearch.ssl.certificateAuthorities: ${ELASTICSEARCH_SSL_CA_CERT_PATH}#" \
       -i /kibana/config/kibana.yml
 else
   sed -e "s/%ELASTICSEARCH_SSL_CA_CERT_PATH_CONFIG_ENTRY%//" \
+      -i /kibana/config/kibana.yml
+fi
+
+if [[ ${ENABLE_KIBANA_RUNAS_HEADER} == true ]]; then
+  sed -e "s#%ELASTICSEARCH_REQUEST_HEADERS_WHITELIST_ENTRY%#elasticsearch.requestHeadersWhitelist: [ es-security-runas-user, authorization ]#" \
+      -e "s#%XPACK_MONITORING_ELASTICSEARCH_REQUEST_HEADERS_WHITELIST_ENTRY%#xpack.monitoring.elasticsearch.requestHeadersWhitelist: [ es-security-runas-user, authorization ]#" \
+      -i /kibana/config/kibana.yml
+
+else
+  sed -e "s/%ELASTICSEARCH_REQUEST_HEADERS_WHITELIST_ENTRY%//" \
+      -e "s/%XPACK_MONITORING_ELASTICSEARCH_REQUEST_HEADERS_WHITELIST_ENTRY%//" \
       -i /kibana/config/kibana.yml
 fi
 
